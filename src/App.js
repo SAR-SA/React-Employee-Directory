c703122b2a94f983b66db0f55a90684af9e3003a
Shawn A. R <sarsa.web@gmail.com>
2020-10-31 21:20:51 -0400

    cdns added

--- src/App.js
+++ src/App.js
@@ -2,38 +2,49 @@ import logo from './logo.svg';
 import React, {Component} from "react";
 import './App.css';
 import getAllEmployees from './utils/Api';
-import Row from './components/Row'
+import Row from './components/Row';
 
-class App extends Component {
-  //const employees = getAllEmployees()
-  state = {
-    exmployees: getAllEmployees()
-  } 
-
-  render() {
-
-  
+function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {this.state.employees.map((employee, index) => {
-          return <>
-        })}
-      </header>
+    <div>
+      <Navbar />
+      <Wrapper>
+        
+      </Wrapper>
     </div>
-  );
-}}
+  )
+}
 
-export default App;
+
+
+// class App extends Component {
+//   //const employees = getAllEmployees()
+//   state = {
+//     employees: getAllEmployees()
+//   } 
+
+//   render() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//         {this.state.employees.map((employee, index) => {
+//           return <>
+//         })}
+//       </header>
+//     </div>
+//   );
+// }}
+
+// export default App;
